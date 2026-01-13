3d67155d2f947aeef7cda3b301d2a21e0da7eb3f
PRED13 <155868501+PRED13@users.noreply.github.com>
2026-01-13 10:10:41 -0600

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     # Esta línea debe coincidir exactamente con el nombre de la función en views.py
-    path('', views.tu_vista_de_procesamiento, name='index'),
+    path('', views.index, name='index'),
 ]
